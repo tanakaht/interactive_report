eb515dcf0dcbbfdf4b28c77dae6756a92dca0767
tanakaht <hirohtht@gmail.com>
2020-11-12 09:33:29 +0900

    作りかけのLinkedTextを除外

--- app/src/LinkedText.js
+++ app/src/LinkedText.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3';
-
+/*
 class LinkedText extends Component {
     constructor(props) {
         super(props);
@@ -148,3 +148,4 @@ class LinkedText extends Component {
 }
 
 export default BubbleChart
+*/
